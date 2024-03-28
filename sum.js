8ccac3f7e6c68805d712b134d2fbf65d73f28a20
Shreyash <shreyash.kumar@hyperrealitytech.com>
2024-03-28 19:01:28 +0530

    Add one more utility function

--- sum.js
+++ sum.js
@@ -2,3 +2,7 @@
 export function sum(a, b) {
   return a + b;
 }
+
+export function multiply(a, b) {
+  return a * b;
+}
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
 // sum.test.js
 import { expect, test } from "vitest";
-import { sum } from "./sum";
+import { sum, multiply } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -13,4 +13,8 @@ test("adds 2 + 2 to equal 4", () => {
 
 test("adds 0 + 0 to equal 0", () => {
   expect(sum(0, 0)).toBe(0);
+});
+
+test("multiplies 2 * 2 to equal 4", () => {
+  expect(multiply(2, 2)).toBe(4);
 });

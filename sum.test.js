f72c6749b7244320c0972bb50c75fc96a1b58ee2
Shreyash <shreyash.kumar@hyperrealitytech.com>
2024-03-28 19:09:23 +0530

    Add divide function tests

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
 // sum.test.js
 import { expect, test } from "vitest";
-import { sum, multiply } from "./sum";
+import { sum, multiply, divide } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -21,4 +21,8 @@ test("multiplies 2 * 2 to equal 4", () => {
 
 test("multiplies 3 * 3 to equal 9", () => {
   expect(multiply(3, 3)).toBe(9);
+});
+
+test("divides 4 / 2 to equal 2", () => {
+  expect(divide(4, 2)).toBe(2);
 });

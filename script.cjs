f7653217d8f4c536a11c43f9eaaa6fad01384453
Shreyash <shreyash.kumar@hyperrealitytech.com>
2024-03-30 14:57:59 +0530

    Add service container for redis

--- script.cjs
+++ script.cjs
@@ -6,7 +6,6 @@ const redis = require("redis");
 const client = redis.createClient({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
 });
 
 client.on("error", (err) => console.log("Redis Client Error", err));

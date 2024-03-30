d7d51e0fe56620d644419a093efab24fe2001bec
Shreyash <shreyash.kumar@hyperrealitytech.com>
2024-03-30 14:19:49 +0530

    Add script

---
+++ script.js
@@ -0,0 +1,25 @@
+// Connect to redis using env variables
+const redis = require("redis");
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+client.on("connect", () => console.log("Redis Client Connected"));
+
+client.connect();
+
+// Set a value
+client.set("mykey", "Hello World!");
+
+setTimeout(() => {
+  client.get("mykey", (err, value) => {
+    console.log(value);
+  });
+}, 3000);
+
+console.log("Redis script");

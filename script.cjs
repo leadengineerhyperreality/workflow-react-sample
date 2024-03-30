f568f261d4bbccf2681f46e8ccae866f5fa9cca7
Shreyash <shreyash.kumar@hyperrealitytech.com>
2024-03-30 15:09:54 +0530

    Add more code

--- script.cjs
+++ script.cjs
@@ -10,18 +10,23 @@ const client = redis.createClient({
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
-client.on("connect", () => console.log("Redis Client Connected to Server"));
+client.on("connect", () => {
+  console.log("Redis Client Connected to Server");
+  client.set("mykey", "Hello World!");
 
-client.connect();
-
-// Set a value
-client.set("mykey", "Hello World!");
-
-setTimeout(() => {
   client.get("mykey", (err, value) => {
     console.log(value);
     client.quit();
   });
+});
+
+client.connect();
+
+// Set a value
+
+setTimeout(() => {
+  console.log("Forced exit");
+  process.exit(0);
 }, 5000);
 
 console.log("Redis script invoked");
